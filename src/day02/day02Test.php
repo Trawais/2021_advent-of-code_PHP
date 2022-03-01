6da2dfaea6dbe52229e54a73a5c23668fb270784
MARKOD01 <david.markovic01@dixonxcarphone.com>
2022-03-01 08:00:28 +0100

    add day2 test - second-star task

--- src/day02/day02Test.php
+++ src/day02/day02Test.php
@@ -8,8 +8,8 @@ final class day02Test extends TestCase
 {
     public function testGivenExamplePasses(): void
     {
-        $measurement = new SubmarinePossition();
+        $measurement = new SubmarinePosition();
         $input = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"];
-        $this->assertEquals(150, $measurement->depthAndPositionChange($input));
+        $this->assertEquals(900, $measurement->depthAndPositionChange($input));
     }
 }
