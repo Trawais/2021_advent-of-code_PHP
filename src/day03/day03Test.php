2955463c168a444025775269009a6aca51b0abff
MARKOD01 <david.markovic01@dixonxcarphone.com>
2022-03-04 10:39:21 +0100

    add day3 test - 2 stars (code duplicity)

---
+++ src/day03/day03Test.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace AdventOfCode\day03;
+
+use PHPUnit\Framework\TestCase;
+
+final class day03Test extends TestCase
+{
+    public function testGivenExamplePasses(): void
+    {
+        $measurement = new BitRates();
+        $input = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"
+        ];
+        $this->assertEquals("230", $measurement->getBitRate($input));
+    }
+}
