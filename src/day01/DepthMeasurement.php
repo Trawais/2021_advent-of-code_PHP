e67bfa0cd6b55677a7fbe06bc3d0b2f04acc1f50
BURESJ02 <josef.bures02@currys.co.uk>
2022-03-07 09:34:03 +0100

    [Add Day01 - part one]

--- src/day01/DepthMeasurement.php
+++ src/day01/DepthMeasurement.php
@@ -2,10 +2,33 @@
 
 namespace AdventOfCode\Day01;
 
+
+use PhpParser\Node\Expr\Array_;
+
 final class DepthMeasurement
 {
-  public function depthMeasurementIncreases(array $input)
-  {
-    return 7;
-  }
+
+    public function readInput($fileName) {
+        $myfile = fopen($fileName, "r") or die("Unable to open file!"); //https://www.w3schools.com/php/php_file_open.asp
+        $loadedData = array();
+        while (!feof($myfile)) {
+            $loadedData[] = (int) fgets($myfile);
+        }
+        return $loadedData;
+    }
+
+    public function depthMeasurementIncreases(array $input): int
+    {
+        $data = $this->readInput("src/day01/input.txt");
+        $previousValue = 0;
+        $incSum = 0;
+        for ($i = 0; $i < (count($data)); $i++){
+        if($data[$i] > $previousValue){
+        $incSum++;
+        }
+        $previousValue = $data[$i];
+        }
+
+        return $incSum - 1;
+    }
 }
--- src/day01/day01Test.php
+++ src/day01/day01Test.php
@@ -2,15 +2,18 @@
 
 namespace AdventOfCode\Day01;
 
-use PHPUnit\Framework\TestCase;
 use AdventOfCode\Day01\DepthMeasurement;
+use PHPUnit\Framework\TestCase;
 
 final class day01Test extends TestCase
 {
     public function testGivenExamplePasses(): void
     {
+
         $measurement = new DepthMeasurement();
-        $input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        $this->assertEquals(7, $measurement->depthMeasurementIncreases($input));
+
+        $input = $measurement->readInput("src/day01/input.txt");
+
+        $this->assertEquals(1502, $measurement->depthMeasurementIncreases($input));
     }
 }
