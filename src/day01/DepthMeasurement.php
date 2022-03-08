a5dddb733d638c6c1a8864f4a7bbfc8e178655dd
BURESJ02 <josef.bures02@currys.co.uk>
2022-03-08 13:24:59 +0100

    [Add day01 - part two]

--- src/day01/DepthMeasurement.php
+++ src/day01/DepthMeasurement.php
@@ -8,27 +8,41 @@ use PhpParser\Node\Expr\Array_;
 final class DepthMeasurement
 {
 
-    public function readInput($fileName) {
+    public function readInput($fileName)
+    {
         $myfile = fopen($fileName, "r") or die("Unable to open file!"); //https://www.w3schools.com/php/php_file_open.asp
         $loadedData = array();
         while (!feof($myfile)) {
-            $loadedData[] = (int) fgets($myfile);
+            $loadedData[] = (int)fgets($myfile);
         }
         return $loadedData;
     }
 
     public function depthMeasurementIncreases(array $input): int
     {
-        $data = $this->readInput("src/day01/input.txt");
         $previousValue = 0;
         $incSum = 0;
-        for ($i = 0; $i < (count($data)); $i++){
-        if($data[$i] > $previousValue){
-        $incSum++;
-        }
-        $previousValue = $data[$i];
+        for ($i = 0; $i < (count($input)); $i++) {
+            if ($input[$i] > $previousValue) {
+                $incSum++;
+            }
+            $previousValue = $input[$i];
         }
 
         return $incSum - 1;
+    }
+
+    public function sumMeasurementIncreases(array $input): int
+    {
+        $incSum = 0;
+        for ($i = 0; $i < count($input) - 3; $i++) {
+            $sum1 = $input[$i] + $input[$i + 1] + $input[$i + 2];
+            $sum2 = $input[$i + 1] + $input[$i + 2] + $input[$i + 3];
+            if ($sum2 > $sum1) {
+                $incSum++;
+            }
+        }
+
+        return $incSum;
     }
 }
--- src/day01/day01Test.php
+++ src/day01/day01Test.php
@@ -7,13 +7,22 @@ use PHPUnit\Framework\TestCase;
 
 final class day01Test extends TestCase
 {
+
     public function testGivenExamplePasses(): void
     {
 
         $measurement = new DepthMeasurement();
-
         $input = $measurement->readInput("src/day01/input.txt");
 
         $this->assertEquals(1502, $measurement->depthMeasurementIncreases($input));
+    }
+
+     public function testPartTwoPasses(): void
+    {
+
+        $measurement = new DepthMeasurement();
+        $input = $measurement->readInput("src/day01/input.txt");
+
+        $this->assertEquals(1538, $measurement->sumMeasurementIncreases($input));
     }
 }
