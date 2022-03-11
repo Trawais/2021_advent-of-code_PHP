e81a0b996a9887fc367227ddb3c52d62e2dc469f
Jan Travnicek <jan.travnicek@currys.co.uk>
2022-03-11 09:06:32 +0100

    add example into test, delete unnecesary variable

--- src/day01/DepthMeasurement.php
+++ src/day01/DepthMeasurement.php
@@ -1,13 +1,9 @@
 <?php declare(strict_types=1);
 
-namespace AdventOfCode\Day01;
-
-
-use PhpParser\Node\Expr\Array_;
+namespace AdventOfCode\day01;
 
 final class DepthMeasurement
 {
-
     public function readInput($fileName)
     {
         $myfile = fopen($fileName, "r") or die("Unable to open file!"); //https://www.w3schools.com/php/php_file_open.asp
@@ -20,25 +16,23 @@ final class DepthMeasurement
 
     public function depthMeasurementIncreases(array $input): int
     {
-        $previousValue = 0;
         $incSum = 0;
-        for ($i = 0; $i < (count($input)); $i++) {
-            if ($input[$i] > $previousValue) {
+        for ($i = 1; $i < (count($input)); $i++) {
+            if ($input[$i] > $input[$i-1]) {
                 $incSum++;
             }
-            $previousValue = $input[$i];
         }
 
-        return $incSum - 1;
+        return $incSum;
     }
 
     public function sumMeasurementIncreases(array $input): int
     {
         $incSum = 0;
         for ($i = 0; $i < count($input) - 3; $i++) {
-            $sum1 = $input[$i] + $input[$i + 1] + $input[$i + 2];
-            $sum2 = $input[$i + 1] + $input[$i + 2] + $input[$i + 3];
-            if ($sum2 > $sum1) {
+            $currentSum = $input[$i] + $input[$i + 1] + $input[$i + 2];
+            $nextSum = $input[$i + 1] + $input[$i + 2] + $input[$i + 3];
+            if ($nextSum > $currentSum) {
                 $incSum++;
             }
         }
--- src/day01/day01Test.php
+++ src/day01/day01Test.php
@@ -1,28 +1,40 @@
 <?php declare(strict_types=1);
 
-namespace AdventOfCode\Day01;
+namespace AdventOfCode\day01;
 
-use AdventOfCode\Day01\DepthMeasurement;
+use AdventOfCode\day01\DepthMeasurement;
 use PHPUnit\Framework\TestCase;
 
 final class day01Test extends TestCase
 {
+    private $measurement;
+    private $input;
+    private $exampleInput;
 
-    public function testGivenExamplePasses(): void
+    protected function setUp(): void
     {
-
-        $measurement = new DepthMeasurement();
-        $input = $measurement->readInput("src/day01/input.txt");
-
-        $this->assertEquals(1502, $measurement->depthMeasurementIncreases($input));
+        $this->measurement = new DepthMeasurement();
+        $this->input = $this->measurement->readInput("src/day01/input.txt");
+        $this->exampleInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
     }
 
-     public function testPartTwoPasses(): void
+    public function testPartOneExamplePasses(): void
     {
+        $this->assertEquals(7, $this->measurement->depthMeasurementIncreases($this->exampleInput));
+    }
 
-        $measurement = new DepthMeasurement();
-        $input = $measurement->readInput("src/day01/input.txt");
+    public function testPartOneSolution(): void
+    {
+        $this->assertEquals(1502, $this->measurement->depthMeasurementIncreases($this->input));
+    }
 
-        $this->assertEquals(1538, $measurement->sumMeasurementIncreases($input));
+    public function testPartTwoExamplePasses(): void
+    {
+        $this->assertEquals(5, $this->measurement->sumMeasurementIncreases($this->exampleInput));
+    }
+
+    public function testPartTwoSolution(): void
+    {
+        $this->assertEquals(1538, $this->measurement->sumMeasurementIncreases($this->input));
     }
 }
