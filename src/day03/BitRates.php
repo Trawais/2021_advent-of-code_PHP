8c0f9c0a2924bacb9636eb6fa292028e7647210a
MARKOD01 <david.markovic01@dixonxcarphone.com>
2022-03-22 09:32:36 +0100

    day03 fix repetition rows

--- src/day03/BitRates.php
+++ src/day03/BitRates.php
@@ -4,75 +4,50 @@ namespace AdventOfCode\day03;
 
 final class BitRates
 {
+    public function returnRightValues($one, $zero, $oneArray, $zeroArray, $zeroOrONe)
+    {
+        if ($zeroOrONe==1) {
+            if ($one >= $zero) {
+                return $oneArray;
+            } else {
+                return $zeroArray;
+            }
+        } else {
+            if ($one >= $zero) {
+                return $zeroArray;
+            } else {
+                return $oneArray;
+            }
+        }
+    }
+
+    public function getOxygenGeneratorOrCo2Rating(array $value, $zeroOrOne): int
+    {
+        $numberOfCharInString = 0;
+        
+        while (count($value) > 1) {
+            $zero = 0;
+            $one = 0;
+            $oneArray = [];
+            $zeroArray = [];
+            for ($numberOfRows = 0; $numberOfRows < count($value); $numberOfRows++) {
+                $split = $value[$numberOfRows];
+                if ($split[$numberOfCharInString] == "0") {
+                    $zero++;
+                    $zeroArray[] .= $split;
+                } else {
+                    $one++;
+                    $oneArray[] .= $split;
+                }
+            }
+            $value = $this->returnRightValues($one, $zero, $oneArray, $zeroArray, $zeroOrOne);
+            $numberOfCharInString++;
+        }
+        return bindec($value[0]);
+    }
+
     public function getBitRate(array $input): int
     {
-        $gama = "";
-        $epsilon = 0;
-        $zero = 0;
-        $one = 0;
-        $oxygenGenerator = $input;
-        $co2Rating = $input;
-        $numberOfCharInString = 0;
-        $zeroArray = [];
-        $oneArray = [];
-
-        while(count($oxygenGenerator)>1) {
-            for ($numberOfRows = 0; $numberOfRows < count($oxygenGenerator); $numberOfRows++) {
-                $split = $oxygenGenerator[$numberOfRows];
-                if ($split[$numberOfCharInString] == "0") {
-                    $zero++;
-                    $zeroArray[] .= $split;
-                } else {
-                    $one++;
-                    $oneArray[] .= $split;
-                }
-            }
-            if ($one >= $zero) {
-                $gama .= "1";
-                $epsilon .= "0";
-                $oxygenGenerator = $oneArray;
-            } else {
-                $gama .= "0";
-                $epsilon .= "1";
-                $oxygenGenerator = $zeroArray;
-            }
-
-            $zero = 0;
-            $one = 0;
-            $oneArray = [];
-            $zeroArray = [];
-            $numberOfCharInString++;
-        }
-
-        $numberOfCharInString = 0;
-
-        while(count($co2Rating)>1) {
-            for ($numberOfRows = 0; $numberOfRows < count($co2Rating); $numberOfRows++) {
-                $split = $co2Rating[$numberOfRows];
-                if ($split[$numberOfCharInString] == "0") {
-                    $zero++;
-                    $zeroArray[] .= $split;
-                } else {
-                    $one++;
-                    $oneArray[] .= $split;
-                }
-            }
-            if ($one >= $zero) {
-                $gama .= "1";
-                $epsilon .= "0";
-                $co2Rating = $zeroArray;
-            } else {
-                $gama .= "0";
-                $epsilon .= "1";
-                $co2Rating = $oneArray;
-            }
-
-            $zero = 0;
-            $one = 0;
-            $oneArray = [];
-            $zeroArray = [];
-            $numberOfCharInString++;
-        }
-        return bindec($oxygenGenerator[0]) * bindec($co2Rating[0]);
+        return $this->getOxygenGeneratorOrCo2Rating($input ,1) * $this->getOxygenGeneratorOrCo2Rating($input, 0);
     }
 }
--- src/day03/day03Test.php
+++ src/day03/day03Test.php
@@ -9,8 +9,7 @@ final class day03Test extends TestCase
     public function testGivenExamplePasses(): void
     {
         $measurement = new BitRates();
-        $input = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"
-        ];
+        $input = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"];
         $this->assertEquals("230", $measurement->getBitRate($input));
     }
 }
